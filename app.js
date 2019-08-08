97c443af2fa7bd5e875168b3cc8184b6c251a9a7
Jake Loew <jakeloew1@gmail.com>
2019-08-08 11:58:25 -0500

    db created sequalize in there

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const layout = require('./views/layout.js')
+// const { db } = require('./models');
+const models = require('./models');
+
+// db.authenticate()
+//     .then(() => {
+//         console.log('connected to the database');
+//     });
 
 const app = express();
 
@@ -17,11 +24,20 @@ app.get('/', (req, res, next) => {
     } catch (err) {
         console.log('error: ' + err)
     }
-    
 })
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const init = async () => {
+    await models.db.sync({force: true});
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}!`)
+    });
+}
+
+init();
+
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
