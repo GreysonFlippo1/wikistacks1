ae0b33620793a7477f7d7238cb2fd62cf6ff8e2c
Jake Loew <jakeloew1@gmail.com>
2019-08-08 11:13:53 -0500

    setup complete

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
-
-app.use(morgan('dev'));
+const bodyParser = require('body-parser')
+const layout = require('./views/layout.js')
 
 const app = express();
 
+app.use(morgan('dev'));
+
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res, next) => {
+    try {
+        res.send(layout());
+    } catch (err) {
+        console.log('error: ' + err)
+    }
+    
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
